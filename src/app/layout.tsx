19c6304d97d0d025f4af6bc27964d6f5d28fa226
Klaus Hintler <klaus.hintler@gmail.com>
2026-05-31 07:57:01 +0200

    feat: add Google Analytics tracking via next/script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -26,7 +27,23 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WHNVJ46DP6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WHNVJ46DP6');
+          `}
+        </Script>
+
         {children}
+        
+        {/* Sitelinks Searchbox JSON-LD */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
